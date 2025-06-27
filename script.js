4196c719079962c14bd40f5714302ad6febfb529
bolsonarokun <nascimento.alves.ana@escola.pr.gov.br>
2025-06-26 22:05:43 -0300

    aula 4 26.06

--- script.js
+++ script.js
@@ -4,10 +4,30 @@ botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave() {
     //alert("Fui clicado!");
-    const texto = documento.querySelector('#entrada-de-texto').value;
+    const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ")
+    const palavras = processaTexto(texto);
 
     campoResultado.textContent = palavras.join(", ");
 }
 
+function processaTexto(texto) {
+    //let palavras = texto.split(/\s+/); "retira espaços" 
+    //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas "
+    let palavras = texto.split("/\P{L}+/u/"); /* \P negação; {L} conjunto de letras; + uma ou mais ocorr~encias; u Unicode*/
+
+    let frequencias = [];
+
+    for ( let i in palavras) {
+        frequencias[i] = 0;
+        for ( let j in palavras) {
+            if(palavras[i]== palavras[j]) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+    
+    return palavras;
+}

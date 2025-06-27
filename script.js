0aa36110f47bc3b6f30789756aba75b959d96179
bolsonarokun <nascimento.alves.ana@escola.pr.gov.br>
2025-06-26 21:27:17 -0300

    aula 2 26.06

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave() {
-    alert("Fui clicado!");
+    //alert("Fui clicado!");
+    const texto = documento.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+
+    campoResultado.textContent = texto;
 }
+
